14b073de88a85dc61807102b32af62c5f041f4c1
Fernando Guillen <fguillen.mail@gmail.com>
2011-07-15 11:03:54 +0200

    trying to load acts_as_mappable

--- config/environment.rb
+++ config/environment.rb
@@ -45,3 +45,5 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
+
+require 'geokit-rails/acts_as_mappable'
